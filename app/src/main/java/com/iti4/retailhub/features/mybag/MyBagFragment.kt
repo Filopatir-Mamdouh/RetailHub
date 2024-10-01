67b0802093826d6de626408eea0cdee3bbb4e370
AhmedElsayed6 <amabrook2022@gmail.com>
2024-10-02 02:03:22 +0300

    Working on card item

---
+++ app/src/main/java/com/iti4/retailhub/features/mybag/MyBagFragment.kt
@@ -0,0 +1,24 @@
+package com.iti4.retailhub.features.mybag
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.iti4.retailhub.R
+
+
+class MyBagFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_bag, container, false)
+    }
+
+
+
+
+
+}
