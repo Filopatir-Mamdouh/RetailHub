a536c4b230129bcac36d9489b5efedea822b58a9
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-10-02 01:09:42 +0300

    create activity authuntication for login and  sign up

---
+++ app/src/main/java/com/iti4/retailhub/loginandsignup/LoginAuthinticationActivity.kt
@@ -0,0 +1,16 @@
+package com.iti4.retailhub.loginandsignup
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.iti4.retailhub.R
+
+class LoginAuthinticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_authintication)
+    }
+}
