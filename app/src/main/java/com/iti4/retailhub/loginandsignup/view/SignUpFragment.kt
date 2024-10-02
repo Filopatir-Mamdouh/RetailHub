723b405090921ca65d275b9828d69bea12c660d3
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-10-02 22:21:05 +0300

    handle on sign up button click to create user

--- app/src/main/java/com/iti4/retailhub/loginandsignup/view/SignUpFragment.kt
+++ app/src/main/java/com/iti4/retailhub/loginandsignup/view/SignUpFragment.kt
@@ -1,21 +1,26 @@
 package com.iti4.retailhub.loginandsignup.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation.findNavController
+import com.iti4.retailhub.MainActivity
 import com.iti4.retailhub.R
 import com.iti4.retailhub.databinding.FragmentSignUpBinding
+import com.iti4.retailhub.loginandsignup.model.UserAuthunticationViewModelViewModel
+import com.iti4.retailhub.userauthuntication.AuthResultState
+import kotlinx.coroutines.launch
 
 class SignUpFragment : Fragment() {
 
     lateinit var signUpBinding: FragmentSignUpBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+val userAuthViewModel: UserAuthunticationViewModelViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +36,31 @@ class SignUpFragment : Fragment() {
         signUpBinding.redArrowGoToLogin.setOnClickListener {
             findNavController(view).navigate(R.id.action_signUpFragment_to_loginInFragment)
         }
+        signUpBinding.sigInBtn.setOnClickListener {
+            val name = signUpBinding.nameTextInput.editText?.text.toString()
+            val email = signUpBinding.emailTextInput.editText?.text.toString()
+            val password = signUpBinding.passowrdTex.editText?.text.toString()
+            userAuthViewModel.createUser(email, password)
+        }
+        lifecycleScope.launch {
+            userAuthViewModel.authStateFlow.collect { authResultState ->
+                when (authResultState) {
+                    is AuthResultState.Loading -> {
+                        signUpBinding.progressBar.visibility = View.VISIBLE
+                    }
+                    is AuthResultState.Success -> {
+                        signUpBinding.progressBar.visibility = View.GONE
+                        val intent= Intent(requireContext(), MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    is AuthResultState.Failure -> {
+                        signUpBinding.progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), authResultState.errorMessage,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+
     }
 
 
