d23ec4aee37658a753c55b50bc45458b81b4fa64
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-10-05 02:55:08 +0300

    added bottom navigation bar

--- app/src/main/java/com/iti4/retailhub/MainActivity.kt
+++ app/src/main/java/com/iti4/retailhub/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.iti4.retailhub.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.navigationView.setupWithNavController(Navigation.findNavController(this,R.id.fragmentContainerView2))
     }
 }
