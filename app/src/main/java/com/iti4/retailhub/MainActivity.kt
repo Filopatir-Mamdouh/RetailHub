7ff16c07772eda8892bbf4fd8a95acf9129e57df
AhmedElsayed6 <amabrook2022@gmail.com>
2024-10-05 21:17:37 +0300

    Fixed Appbar

--- app/src/main/java/com/iti4/retailhub/MainActivity.kt
+++ app/src/main/java/com/iti4/retailhub/MainActivity.kt
@@ -2,13 +2,11 @@ package com.iti4.retailhub
 
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.iti4.retailhub.communicators.ToolbarController
 import com.iti4.retailhub.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,27 +14,43 @@ import kotlin.math.abs
 
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() , ToolbarController {
-    private lateinit var binding : ActivityMainBinding
+class MainActivity : AppCompatActivity(), ToolbarController {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setNavigationIcon(R.drawable.ic_back)
+        binding.toolbar.setNavigationOnClickListener {
+            Navigation.findNavController(
+                this,
+                R.id.fragmentContainerView2
+            ).navigateUp()
+        }
+
+        supportActionBar?.title = ""
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.navigationView.setupWithNavController(Navigation.findNavController(this,R.id.fragmentContainerView2))
+        binding.navigationView.setupWithNavController(
+            Navigation.findNavController(
+                this,
+                R.id.fragmentContainerView2
+            )
+        )
         binding.appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
             val totalScrollRange = appBarLayout.totalScrollRange
             if (abs(verticalOffset.toDouble()) >= totalScrollRange) {
                 // Collapsed
+                binding.toolbar.setBackgroundColor(getResources().getColor(R.color.white))
                 binding.pageName.visibility = View.GONE
                 binding.collapsedPageName.visibility = View.VISIBLE
             } else {
                 // Expanded
+                binding.toolbar.setBackgroundColor(getResources().getColor(R.color.background_color))
                 binding.pageName.visibility = View.VISIBLE
                 binding.collapsedPageName.visibility = View.GONE
             }
@@ -57,4 +71,6 @@ class MainActivity : AppCompatActivity() , ToolbarController {
             collapsedPageName.text = title
         }
     }
+
+
 }
