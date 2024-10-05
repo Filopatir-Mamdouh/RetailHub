2f1c10965a24a769caecfb42402f88490aa70ef0
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-10-05 13:58:25 +0300

    added appbar for the application

--- app/src/main/java/com/iti4/retailhub/MainActivity.kt
+++ app/src/main/java/com/iti4/retailhub/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.iti4.retailhub
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.iti4.retailhub.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.abs
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -25,5 +28,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         binding.navigationView.setupWithNavController(Navigation.findNavController(this,R.id.fragmentContainerView2))
+        binding.appBar.addOnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val totalScrollRange = appBarLayout.totalScrollRange
+            if (abs(verticalOffset.toDouble()) >= totalScrollRange) {
+                // Collapsed
+                binding.pageName.visibility = View.GONE
+                binding.collapsedPageName.visibility = View.VISIBLE
+            } else {
+                // Expanded
+                binding.pageName.visibility = View.VISIBLE
+                binding.collapsedPageName.visibility = View.GONE
+            }
+        }
     }
 }
