27ac1be56ad25bdd59ba1b7504c3765cc373a35d
abdelrahman <abdelrahmanmostafa8171@gmail.com>
2024-10-08 20:26:57 +0300

    add search and filter ui

---
+++ app/src/main/java/com/iti4/retailhub/search/viewmodels/FilterViewModel.kt
@@ -0,0 +1,9 @@
+package com.iti4.retailhub.features.shop.viewmodels
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FilterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
