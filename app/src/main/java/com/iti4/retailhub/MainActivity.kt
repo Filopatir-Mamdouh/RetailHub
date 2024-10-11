e865b2eebac821add84c867e545b2f56254b08a4
Filopatir-Mamdouh <96422494+Filopatir-Mamdouh@users.noreply.github.com>
2024-10-12 00:32:07 +0300

    finished checking on network state

--- app/src/main/java/com/iti4/retailhub/MainActivity.kt
+++ app/src/main/java/com/iti4/retailhub/MainActivity.kt
@@ -5,33 +5,39 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.get
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
 import com.iti4.retailhub.databinding.ActivityMainBinding
-import com.iti4.retailhub.features.address.AddressViewModel
+import com.iti4.retailhub.logic.NetworkUtils
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val sharedViewModel: AddressViewModel by viewModels()
+    private val _isConnectedToNetwork = MutableStateFlow(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        val isConnectedToNetwork =_isConnectedToNetwork.onStart { checkNetwork() }.stateIn(lifecycleScope, SharingStarted.Eagerly, false)
         binding.navigationView.apply {
             setOnItemSelectedListener { menuItem ->
                 val navController = Navigation.findNavController(this@MainActivity, R.id.fragmentContainerView2)
@@ -42,6 +48,30 @@ class MainActivity : AppCompatActivity() {
                     true
                 } else {
                     false
+                }
+            }
+        }
+        val fragments = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)?.childFragmentManager?.fragments
+        lifecycleScope.launch {
+            isConnectedToNetwork.collect{ networkState ->
+                if(networkState){
+                    fragments?.forEach{
+                        it.view?.isEnabled = true
+                    }
+                    findViewById<View>(R.id.fragmentContainerView2).visibility = View.VISIBLE
+                    binding.apply{
+                        navigationView.menu.setGroupEnabled(R.id.navGroup, true)
+                        networkAnimView.visibility = View.GONE
+                    }
+                }else{
+                    fragments?.forEach{
+                        it.view?.isEnabled = false
+                    }
+                    findViewById<View>(R.id.fragmentContainerView2).visibility = View.GONE
+                    binding.apply{
+                        navigationView.menu.setGroupEnabled(R.id.navGroup, false)
+                        networkAnimView.visibility = View.VISIBLE
+                    }
                 }
             }
         }
@@ -74,5 +104,12 @@ class MainActivity : AppCompatActivity() {
         binding.navigationView.visibility = View.VISIBLE
     }
 
-
+    private fun checkNetwork() {
+        lifecycleScope.launch {
+            while (true) {
+                _isConnectedToNetwork.emit(NetworkUtils.isNetworkAvailable(this@MainActivity))
+                delay(3000)
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/iti4/retailhub/logic/NetworkUtils.kt
@@ -0,0 +1,13 @@
+package com.iti4.retailhub.logic
+import android.content.Context
+import android.net.ConnectivityManager
+
+object NetworkUtils {
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return capabilities != null
+    }
+}
